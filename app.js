485e17577ddf7de7daef33b6faa816091066f253
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2022-03-24 18:02:07 -0700

    theme testing
    testing out foam theme/ web design

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-let card_title_value ="";
-let title_card =document.getElementById('title-card').innerHTML=card_title_value;
-let card_desc = document.getElementById('card-paragraph').innerHTML=card_title_value;
+let card_title_value = "";
+let title_card = (document.getElementById("title-card").innerHTML =
+  card_title_value);
+let card_desc = (document.getElementById("card-paragraph").innerHTML =
+  card_title_value);
 
-    
-
-    //title_card =document.getElementById('title-card').innerHTML=userInput;
+//title_card =document.getElementById('title-card').innerHTML=userInput;
 //not necessary but only for debugging purposes
 
-let simpleAlert = () =>{
-    alert("Simple alert message");
-}
+let simpleAlert = () => {
+  alert("Simple alert message");
+};
 
- let addNewCardTitle = () =>{
-     //using userInput as a capture device
-    let userInput = prompt("Choose A Title");
+let addNewCardTitle = () => {
+  //using userInput as a capture device
+  let userInput = prompt("Choose A Title");
 
-    
-        title_card =document.getElementById('title-card').innerHTML=userInput;
-    
-     userInput = prompt("Choose A Description");
-             
-        title_card =document.getElementById('card-paragraph').innerHTML=userInput;
-     
-    
- }
+  title_card = document.getElementById("title-card").innerHTML = userInput;
 
-let killCard = () =>{
-    alert("Card is resetted");
-    title_card =document.getElementById('title-card').innerHTML=card_title_value;
-    card_desc = document.getElementById('card-paragraph').innerHTML=card_title_value;
-}
+  userInput = prompt("Choose A Description");
+
+  title_card = document.getElementById("card-paragraph").innerHTML = userInput;
+};
+
+let killCard = () => {
+  alert("Card is resetted");
+  title_card = document.getElementById("title-card").innerHTML =
+    card_title_value;
+  card_desc = document.getElementById("card-paragraph").innerHTML =
+    card_title_value;
+};
