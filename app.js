8adced6b982a2c9ac9dbf977cc8c48feca60ef6c
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2022-04-18 13:51:35 -0700

    Read me updates (major)
    #Renamed Title
    #removed unnecessary elements
    #Create function is disabled for now

--- app.js
+++ app.js
@@ -31,21 +31,22 @@ function killCard() {
 }
 // below are functions needed to be execute the settings panel
 
-function openCreate() {
-  let blob = "Leo";
-  //FOR DEBUGGING alert("You've pressed create");
-  document.getElementById(
-    "content_wrapper"
-  ).innerHTML = `<div id="cloned_card" class="card-container"></div>`;
-  document.getElementById(
-    "cloned_card"
-  ).innerHTML = `<h2 id="title-card">Title</h2> <p id="card-paragraph">text content</p>`;
-  document.getElementById(
-    "card-paragraph"
-  ).innerHTML = `<ul class="card-options-ul">
-<li onclick="addNewCardTitle()" class="card-options-li">New Card</li>
+// function openCreate() {
+//   let blob = "Leo";
+//   //FOR DEBUGGING alert("You've pressed create");
+//   userInput = prompt("Choose A Description");
+//   document.getElementById(
+//     "content_wrapper"
+//   ).innerHTML = `<div id="cloned_card" class="card-container"></div>`;
+//   document.getElementById(
+//     "cloned_card"
+//   ).innerHTML = `<h2 id="title-card">Title</h2> <p id="card-paragraph">text content</p>`;
+//   document.getElementById(
+//     "card-paragraph"
+//   ).innerHTML = `<ul class="card-options-ul">
+// <li onclick="addNewCardTitle()" class="card-options-li">New Card</li>
 
-<li onclick="killCard()" class="card-options-li">Remove Card</li>
+// <li onclick="killCard()" class="card-options-li">Remove Card</li>
 
-</ul>`;
-}
+// </ul>`;
+// }
